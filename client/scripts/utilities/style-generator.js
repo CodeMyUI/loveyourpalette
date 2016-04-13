820ae116fbc17002a950b10e7405fbdc2bb010ae
Nicholas Dobie <dobienick@gmail.com>
2016-04-12 22:16:09 -0500

    Removed the use of PostCSS to speed up generation.

--- client/scripts/utilities/style-generator.js
+++ client/scripts/utilities/style-generator.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const postcss = require('postcss');
-//
-// // PostCSS Extensions
-const autoprefixer = require('autoprefixer');
-const cssVariables = require('postcss-css-variables');
+const TinyColor = require('tinycolor2');
 
 const store = require('../store');
 const colorStyleSheet = require('!!raw!../../styles/colors.css');
@@ -12,24 +8,31 @@ const colorStyleSheet = require('!!raw!../../styles/colors.css');
 let lypStyleTag = document.createElement('style');
 document.getElementsByTagName('head')[0].appendChild(lypStyleTag);
 
+let varRegEx = /var\(([^\)]+)\)/gm;
+
 let generateStyleSheet = function (palette) {
 
-  postcss([
-    cssVariables({
-      variables: {
-        '--color-primary': palette.primary,
-        '--color-accent': palette.accent,
-        '--color-dark-gray': palette.darkGray,
-        '--color-light-gray': palette.lightGray,
-        '--color-white': palette.white
-      }
-    }),
-    autoprefixer()
-  ])
-  .process(colorStyleSheet)
-  .then(function (result) {
-    lypStyleTag.innerText = result.css;
+  let variables =  {
+    '--color-primary': palette.primary,
+    '--color-primary-readable': TinyColor.mostReadable(palette.primary, [palette.darkGray, palette.white]),
+    '--color-accent': palette.accent,
+    '--color-accent-readable': TinyColor.mostReadable(palette.accent, [palette.darkGray, palette.white]),
+    '--color-dark-gray': palette.darkGray,
+    '--color-dark-gray-readable': TinyColor.mostReadable(palette.darkGray, [palette.darkGray, palette.white]),
+    '--color-light-gray': palette.lightGray,
+    '--color-light-gray-readable': TinyColor.mostReadable(palette.lightGray, [palette.darkGray, palette.white]),
+    '--color-white': palette.white,
+    '--color-white-readable': TinyColor.mostReadable(palette.white, [palette.darkGray, palette.white])
+  };
+
+  let finishedStyleSheet = colorStyleSheet.replace(varRegEx, function (varStatement, variableName) {
+    if (variableName in variables) {
+      return variables[variableName];
+    }
+    return varStatement;
   });
+
+  lypStyleTag.innerText = finishedStyleSheet;
 
 }
 
